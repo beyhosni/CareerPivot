b4426d6bfc9e52f13af285baa662e786f131fce0
beyhosni <beyhosni@gmail.com>
2025-12-22 18:41:35 +0100

    feat:add swagger apenapi documentation

---
+++ backend/src/main/java/com/careerpivot/backend/config/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package com.careerpivot.backend.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI careerPivotOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("CareerPivot API")
+                        .description("Documentation interactive des APIs de la plateforme CareerPivot")
+                        .version("v1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
